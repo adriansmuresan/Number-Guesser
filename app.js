a0c3a21296342518383e11478121a82b95c7bfd8
Adrian S MUresan <adriansmuresan@gmail.com>
2019-09-13 17:35:31 -0500

    Add/select  variables that we will be working with, and breef description

---
+++ app.js
@@ -0,0 +1,22 @@
+/*
+Game function:
+- Player must guess a number between a min and max
+- Player gets a certain amount of guesses
+- Notify palyer of guesses remaining
+- Notify player of the correct answer if loose
+- Let palyer choose to play again
+*/
+
+// Game values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI Elements
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBTN = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
